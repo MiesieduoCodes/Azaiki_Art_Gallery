a327238de99cbe0bed8bc92f3d32e5a69bd11224
Miesieduo Veria <miesieduoveria@gmail.com>
2025-04-29 08:28:11 +0100

    Add

--- app/founder/page.tsx
+++ app/founder/page.tsx
@@ -169,7 +169,7 @@ const Page = () => {
                 className="object-cover"
               />
               <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-4">
-                <p className="text-sm"></p>
+                <p className="text-sm">Celebrating An Unassuming Brain Box, Hon. Steve Azaiki, OON</p>
               </div>
             </div>
             <div className="relative h-64 rounded-lg overflow-hidden shadow-lg">
